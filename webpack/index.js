43618b1d0fbbaaf867c78ab2546dbb1ac6edc155
Mayabits <maylagoulue@gmail.com>
2018-03-21 22:29:28 +0100

    configurando webpack

---
+++ webpack/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const container = document.getElementById("app");
+
+const holaMundo = <h1>hola mundo</h1>;
+ReactDOM.render(holaMundo , app);
